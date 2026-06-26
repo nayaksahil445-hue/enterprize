27fafb0fce014612e5b1fce2def8b33444c048f7
Sahil Nayak <nayaksahil445@example.com>
2026-06-26 13:31:09 +0530

    fix: make navbar consistent on scroll - no style change

--- src/main.js
+++ src/main.js
@@ -41,8 +41,6 @@ window.addEventListener('load', () => {
    ================================================================ */
 const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 60) navbar.classList.add('scrolled');
-  else navbar.classList.remove('scrolled');
   updateScrollTop();
 });
 
